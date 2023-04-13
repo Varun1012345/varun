162f2897cec8b3c932e132d43da73b6fdddbbafb
Varun1012345 <130635147+Varun1012345@users.noreply.github.com>
2023-04-13 09:15:42 +0000

    nothing much

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 print("hello world")
+name = input("enter your name")
+time= float(input("enter the current time"))
+if (time<12):
+ print("good morning",name)
+elif(time>=16):
+ print("good afternoon",name)
+elif(time<=20):
+ prit("good evening",name)
+else
+print("good night",name)
